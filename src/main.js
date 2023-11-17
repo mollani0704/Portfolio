a15639ccc696b477950b01e749bd272e59d72035
mollani0704 <mollani0704@gmail.com>
2023-11-17 12:21:49 +0900

    [fix] 문구 수정

--- src/main.js
+++ src/main.js
@@ -43,40 +43,40 @@ navbarMenu.addEventListener('click', () => {
 ('use strict');
 
 // 프로젝트 필터링 관련 로직 처리
-const categories = document.querySelector('.categories');
-const projects = document.querySelectorAll('.project');
-const projectsContainer = document.querySelector('.projects');
+// const categories = document.querySelector('.categories');
+// const projects = document.querySelectorAll('.project');
+// const projectsContainer = document.querySelector('.projects');
 
-categories.addEventListener('click', event => {
-    const filter = event.target.dataset.category;
-    if (filter == null) {
-        return;
-    }
+// categories.addEventListener('click', event => {
+//     const filter = event.target.dataset.category;
+//     if (filter == null) {
+//         return;
+//     }
 
-    handleActiveSelection(event.target);
-    filterProjects(filter);
-});
+//     handleActiveSelection(event.target);
+//     filterProjects(filter);
+// });
 
-function handleActiveSelection(target) {
-    const active = document.querySelector('.category--selected');
-    active.classList.remove('category--selected');
-    target.classList.add('category--selected');
-}
+// function handleActiveSelection(target) {
+//     const active = document.querySelector('.category--selected');
+//     active.classList.remove('category--selected');
+//     target.classList.add('category--selected');
+// }
 
-function filterProjects(filter) {
-    projectsContainer.classList.add('anim-out');
-    projects.forEach(project => {
-        if (filter == 'all' || filter === project.dataset.type) {
-            project.style.display = 'block';
-        } else {
-            project.style.display = 'none';
-        }
-    });
+// function filterProjects(filter) {
+//     projectsContainer.classList.add('anim-out');
+//     projects.forEach(project => {
+//         if (filter == 'all' || filter === project.dataset.type) {
+//             project.style.display = 'block';
+//         } else {
+//             project.style.display = 'none';
+//         }
+//     });
 
-    setTimeout(() => {
-        projectsContainer.classList.remove('anim-out');
-    }, 250);
-}
+//     setTimeout(() => {
+//         projectsContainer.classList.remove('anim-out');
+//     }, 250);
+// }
 
 // const navbar = document.querySelector('#navbar');
 
